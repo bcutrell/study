87a5a847c236ba34fde4661863fea9785f483907
Ben Cutrell <bcutrell@Bens-Macbook.local>
2018-03-12 17:09:49 -0400

    adds numpy exercises

---
+++ python/financial-analysis-and-trading/numpy-exercises.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+# create an array of 10 zeros 
+np.zeros(10)
+
+# create an array of 10 ones
+np.ones(10)
+
+# create an array of the integers from 10 to 50
+np.arange(10,50)
+
+# create an array of all the even integers from 10 to 50
+np.arange(10,50,2)
+
+# create a 3x3 matrix with values ranging from 0 to 8
+np.arange(0,9).reshape(3,3)
+
+# create a 3x3 identity matrix
+np.eye(3)
+
+# use NumPy to generate a random number between 0 and 1
+
+# use NumPy to generate an array of 25 random numbers sampled from a standard normal distribution
+
+# create an array of 20 linearly spaced points between 0 and 1:
+
+# given matrix
+mat = np.arange(1,26).reshape(5,5)
+
+# reproduce output:
+
+# array([[12, 13, 14, 15],
+#        [17, 18, 19, 20],
+#        [22, 23, 24, 25]])
+
+
+# 20
+
+# array([[ 2],
+#        [ 7],
+#        [12]])
+
+
+array([21, 22, 23, 24, 25])
+
+
+# array([[16, 17, 18, 19, 20],
+#        [21, 22, 23, 24, 25]])
+
+# get the sum of all the values in mat
+
+# get the standard deviation of the values in mat
+
+# get the sum of all the columns in mat
