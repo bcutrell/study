07b99b376a4a90b0d6d4d48ca0442e8ac6bbdcb0
Ben Cutrell <bcutrell@dhcp-a-023.int.smartleaf.com>
2018-03-14 15:14:52 -0400

    finishes numpy exercises

--- python/financial-analysis-and-trading/numpy-exercises.py
+++ python/financial-analysis-and-trading/numpy-exercises.py
@@ -19,36 +19,51 @@ np.arange(0,9).reshape(3,3)
 np.eye(3)
 
 # use NumPy to generate a random number between 0 and 1
+np.random.rand()
 
 # use NumPy to generate an array of 25 random numbers sampled from a standard normal distribution
+np.random.randn(25)
 
 # create an array of 20 linearly spaced points between 0 and 1:
+np.linspace(0, 1,20)
 
 # given matrix
 mat = np.arange(1,26).reshape(5,5)
 
 # reproduce output:
 
+mat[2:,1:]
 # array([[12, 13, 14, 15],
 #        [17, 18, 19, 20],
 #        [22, 23, 24, 25]])
 
 
+mat[3,4]
 # 20
 
+mat[:3,1:2]
 # array([[ 2],
 #        [ 7],
 #        [12]])
 
 
-array([21, 22, 23, 24, 25])
+mat[4,:]
+# array([21, 22, 23, 24, 25])
 
-
+mat[3:,:]
 # array([[16, 17, 18, 19, 20],
 #        [21, 22, 23, 24, 25]])
 
 # get the sum of all the values in mat
+mat.sum()
 
 # get the standard deviation of the values in mat
+mat.std()
 
 # get the sum of all the columns in mat
+mat.sum(axis=0)
+
+# What is the best way to ensure you get the same random numbers
+np.random.seed(101)
+
+
