2b229c7cba81a82344b12830601916e7ac2f2fb0
Ben Cutrell <bcutrell@Bens-Macbook.local>
2018-03-21 07:57:36 -0400

    starts pandas exercises

---
+++ python/financial-analysis-and-trading/pandas-exercises.py
@@ -0,0 +1,39 @@
+
+# Import pandas and read in the banklist.csv file into a dataframe called banks.
+
+# Show the head of the dataframe
+
+
+# What are the column names?
+ 
+
+# How many States (ST) are represented in this data set?
+
+ 
+# Get a list or array of all the states in the data set.
+
+ 
+# What are the top 5 states with the most failed banks?
+
+ 
+# What are the top 5 acquiring institutions?
+
+ 
+# How many banks has the State Bank of Texas acquired? How many of them were actually in Texas?
+
+ 
+# What is the most common city in California for a bank to fail in?
+
+ 
+# How many failed banks don't have the word "Bank" in their name?
+ 
+ 
+# How many bank names start with the letter 's' ?
+
+ 
+# How many CERT values are above 20000 ?
+ 
+# How many bank names consist of just two words? (e.g. "First Bank" , "Bank Georgia" )
+
+ 
+# Bonus: How many banks closed in the year 2008?
