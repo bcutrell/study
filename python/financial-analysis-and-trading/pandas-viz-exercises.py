6f787debb2e369d85a9649a226e8cc970cc3b132
Ben Cutrell <bcutrell@Bens-Macbook.local>
2018-03-23 16:10:09 -0400

    adds pandas viz exercises

--- python/financial-analysis-and-trading/pandas-viz-exercises.py
+++ python/financial-analysis-and-trading/pandas-viz-exercises.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 df3 = pd.read_csv('df3')
@@ -24,3 +25,42 @@ df3 = pd.read_csv('df3')
 exit()
 
 # Notes
+import seaborn as sns # improves styles
+df = pd.read_csv('df1', index_col=0)
+df['A'].hist(bins=30)
+
+df.plot.bar() # uses index as category stacked=True
+df['A'].plot.hist(bins=50)
+df.plot.line(x=df.index, y='B', figsize=(12,3), lw=1)
+
+df.plot.scatter(x='A',y='B',c='C',cmap='cwarm') # colormap
+# s=df['C']*100 # sets size of markers
+
+df.plot.box()
+df.plot.hexbin(x='a', y='b', gridsize=25, cmap='coolwarm')
+
+df.plot.kde() # density
+
+# Time Series
+df = pd.read_csv(parse_dates=True, index_col='Date',)
+df.plot(xlim=[start_date, end_date], ylim=[start, end])
+df.loc['start_date':'end_date'][column]
+
+fig, ax = plt.subplots()
+fig.autofmt_xdate() # fix overlap
+ax.plot_date(idx, array, '-')
+
+ax.yaxis.grid(True)
+ax.xaxis.grid(True)
+
+plt.tight_layout()
+
+
+
+ax.xaxis.set_major_locator(dates.MonthLocator()) # set tick for every month
+ax.xaxis.set_major_formatter(dates.DateFormatter('\n\n\n%b-%Y')) # set tick format use \n to add space
+
+ax.xaxis.set_minor_locator(dates.WeekdayLocator(byweekday=0)) # byweekday=0 Monday
+ax.xaxis.set_minor_formatter(dates.DateFormatter('%a'))
+
+%matplotlib notebook # adds zooming tool
