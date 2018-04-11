869c767739d695d3332b2169d9e3711c3818a7a6
Ben Cutrell <bcutrell@Bens-Macbook.local>
2018-04-11 14:43:37 -0400

    adds simple python trading algo

---
+++ python/financial-analysis-and-trading/quantopian/bollinger-bands.py
@@ -0,0 +1,14 @@
+'''
+20 day rolling mean and 20 day rolling std dev
+
+20 day mean +/- (2 * 20 day std dev)
+
+Use bands and compare them to the current price as signal
+
+Check Daily
+  If above:
+    short stock
+  If below:
+    long stock
+'''
+
