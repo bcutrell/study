924912c2b1620e21dcad09db72462c60e0009cb5
Ben Cutrell <bcutrell@Bens-Macbook.local>
2018-04-12 13:32:00 -0400

    adds future notes

---
+++ python/financial-analysis-and-trading/quantopian/futures.py
@@ -0,0 +1,17 @@
+# Derivative - a contract between two or more parties whose value 
+# is based on an agreed upon underlying financial asset, index, or security. 
+# Common underlying instruments: bonds, commodities, 
+# currencies, interest rates, market indexes, and stocks
+
+# Forward contact - agreement between two parties to pay a delivery price k
+# for some asset at some point in the future
+
+# the person selling the asset is short
+# Short position:
+# K - St
+# Long position:
+# St - K
+
+# Futures are forwards that have been standardized on an exchange
+
+# Consecutive vs Continuous Futures
