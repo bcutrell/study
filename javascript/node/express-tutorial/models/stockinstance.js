1e6ef84f4786e436f41c516aa900fea07710ce45
Ben Cutrell <bcutrell@Bens-Macbook.local>
2018-05-10 09:08:48 -0400

    adds schema

---
+++ javascript/node/express-tutorial/models/stockinstance.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var StockInstanceSchema = new Schema(
+  {
+    stock: { type: Schema.ObjectId, ref: 'Stock', required: true },
+    close_price: { type: Number, required: true }
+    date:   { type: Date },
+  }
+);
+
+module.exports = mongoose.model('StockInstance', StockInstanceSchema);
+
