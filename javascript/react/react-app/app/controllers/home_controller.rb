0257dcbbb07cf5665917e4cb3209c3c520101dbb
Ben Cutrell <bcutrell@dhcp-a-074.int.smartleaf.com>
2018-08-13 15:46:37 -0400

    adds notes

--- javascript/react/react-app/app/controllers/home_controller.rb
+++ javascript/react/react-app/app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+
   def index
 
     # initial state
@@ -7,6 +8,6 @@ class HomeController < ApplicationController
       holdings: [],
       target: nil
     }
-
+ 
   end
 end
