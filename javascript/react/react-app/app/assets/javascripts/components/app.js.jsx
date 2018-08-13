224cffea802552d4ea60b06f5fa05e5f91d320f8
Ben Cutrell <bcutrell@dhcp-a-074.int.smartleaf.com>
2018-08-13 10:13:43 -0400

    working redux example

--- javascript/react/react-app/app/assets/javascripts/components/app.js.jsx
+++ javascript/react/react-app/app/assets/javascripts/components/app.js.jsx
@@ -27,27 +27,43 @@ class Account extends React.Component {
     return (
       <div>
         <h2>Aciton Counter: {this.props.ctr}</h2>
-        <p>{this.props.account.name}</p>
+        <p>Account Name: {this.props.account.name}</p>
         <button onClick={this.props.addHolding}>Add Holding</button>
         <button onClick={this.props.addTarget}>Add Target</button>
         <p> Target: {this.props.account.target ? this.props.account.target.name : 'No Target'}</p>
+        <h2> Holdings </h2>
         <ul> { holdings } </ul>
       </div>
     )
   }
 }
 
-// **Must be Component in order to recieve State from Redux
+// ** Must be Component in order to recieve State from Redux
 class Target extends React.Component {
 
   genAllocation() {
-
+    text = ""
+    const nHoldings = this.props.account.holdings.length
+    if (nHoldings > 0) {
+      let weight = 100;
+      
+      text = this.props.account.holdings.map((holding, index) => {
+        return (
+          <li> {holding.ticker} {weight / nHoldings} </li>
+        )
+      })
+    }
+    return text;
   }
 
   render() {
+    allocation = this.genAllocation();
 
     return (
-      <h2>Target Allocation</h2>
+      <div>
+        <h2>Target Allocation</h2>
+        <ul> {allocation} </ul>
+      </div>
     )
   }
 }
