6f6910a98b5323ecc1634f9976a07479ad91e599
Ben Cutrell <bcutrell@dhcp-a-074.int.smartleaf.com>
2018-08-15 14:55:42 -0400

    small performance tweak

--- javascript/react/burger-app/src/components/Burger/OrderSummary/OrderSummary.js
+++ javascript/react/burger-app/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import classes from './OrderSummary.css';
 
 import Button from '../../UI/Button/Button';
 import Aux from '../../../hoc/Aux';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-            return <li key={igKey}><span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}</li>
-        });
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <ul>
-                { ingredientSummary }
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinue}>CONTINUE</Button>
-        </Aux>
-    );
-    
+class OrderSummary extends Component {
+    // this could be a functional component
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(igKey => {
+                return <li key={igKey}><span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}</li>
+            });
+
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <ul>
+                    { ingredientSummary }
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinue}>CONTINUE</Button>
+            </Aux>
+        );
+    }    
 }
 
-export default orderSummary;
+export default OrderSummary;
