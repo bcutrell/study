b7e24d13d7c9e829e587fdc4cdfcb5c335b93843
Ben Cutrell <bcutrell@dhcp-a-074.int.smartleaf.com>
2018-08-22 15:14:11 -0400

    refactors reducers

--- javascript/react/burger-app/src/containers/Checkout/Checkout.js
+++ javascript/react/burger-app/src/containers/Checkout/Checkout.js
@@ -4,9 +4,15 @@ import { connect } from 'react-redux';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
 
-import { Route } from 'react-router-dom'
+import { Route, Redirect } from 'react-router-dom';
+import * as actions from '../../store/actions/index';
+
 
 class Checkout extends Component {
+
+    componentWillMount() {
+        this.props.onInitPurchase();
+    }
 
     onCheckoutContinued = () => {
         this.props.history.replace('/checkout/contact-data');
@@ -15,25 +21,41 @@ class Checkout extends Component {
     onCheckoutCancelled = () => {
         this.props.history.goBack();
     }
-    
+
     render() {
-        return (
-            <div>
-                <CheckoutSummary 
+        let summary = <Redirect to="/" />
+
+        if (this.props.ings) {
+            const purchasedRedirect =this.props.purchased ? <Redirect to="/" /> : null
+            summary = (
+                <div>
+                    { purchasedRedirect }
+                    <CheckoutSummary
                     onCheckoutCancelled={this.onCheckoutCancelled}
                     onCheckoutContinued={this.onCheckoutContinued}
                     ingredients={this.props.ings} />
-                <Route path={this.props.match.url + "/contact-data"} 
-                    component={ContactData}/>
-            </div>
-        )}
+                    <Route path={this.props.match.url + "/contact-data"}
+                        component={ContactData}/>
+                </div>
+            )
+        }
+
+        return summary
+    }
 }
 
 
 const mapStateToProps = state => {
     return {
-        ings: state.ingredients
+        ings: state.burgerBuilder.ingredients,
+        purchased: state.order.purchased
     };
 }
 
-export default connect(mapStateToProps)(Checkout);
+const mapDispatchToProps = dispatch => {
+    return {
+        onInitPurchase: () => dispatch(actions.purchaseInit())
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Checkout);
--- javascript/react/burger-app/src/containers/Checkout/ContactData/ContactData.js
+++ javascript/react/burger-app/src/containers/Checkout/ContactData/ContactData.js
@@ -194,16 +194,16 @@ class ContactData extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.ingredients,
-        price: state.totalPrice,
-        loading: state.loading
+        ings: state.burgerBuilder.ingredients,
+        price: state.burgerBuilder.totalPrice,
+        loading: state.order.loading
     };
 }
 
 const mapDispatchToProps = dispath => {
     return {
-        onOrderBurger: (orderData) => dispath(actions.purchaseBurger())
+        onOrderBurger: (orderData) => dispath(actions.purchaseBurger(orderData))
     }
 }
 
-export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
