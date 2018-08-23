f46b8145a9b292f775a8fb1ba14d76e25ca2f85e
Ben Cutrell <bcutrell@dhcp-a-074.int.smartleaf.com>
2018-08-23 08:32:54 -0400

    adds sign in

--- javascript/react/burger-app/src/index.js
+++ javascript/react/burger-app/src/index.js
@@ -10,11 +10,13 @@ import registerServiceWorker from './registerServiceWorker';
 
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
+import authReducer from './store/reducers/auth';
 import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
-    order: orderReducer
+    order: orderReducer,
+    auth: authReducer
 })
 
 
