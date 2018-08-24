a853dcc9bd2afb2d2f0b3a0c681c682b740bcc74
Ben Cutrell <bcutrell@dhcp-a-074.int.smartleaf.com>
2018-08-24 09:36:17 -0400

    bug fixes

--- javascript/react/burger-app/src/store/actions/order.js
+++ javascript/react/burger-app/src/store/actions/order.js
@@ -25,7 +25,7 @@ export const purchaseBurgerStart = () => {
 export const purchaseBurger = (orderData, token) => {
     return dispatch => {
         dispatch(purchaseBurgerStart());
-        axios.post('/orders.json' + token, orderData)
+        axios.post('/orders.json?auth=' + token, orderData)
             .then(response => {
                 dispatch(purchaseBurgerSuccess(response.data.name, orderData));
             })
@@ -64,7 +64,7 @@ export const fetchOrdersStart = () => {
 export const fetchOrders = (token, userId) => {
     return dispatch => {
         dispatch(fetchOrdersStart());
-        const queryParams = '?auth=' + token + '&orderby="userId"&equalTo="' + userId + '"';
+        const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
         axios.get( '/orders.json' + queryParams  )
             .then( res => {
                 const fetchedOrders = [];
