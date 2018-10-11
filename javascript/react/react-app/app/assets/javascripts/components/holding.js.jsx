9942a0fad0f1ae47982ad20166d3f639df7416d2
Ben Cutrell <bcutrell@dhcp-a-074.int.smartleaf.com>
2018-10-11 09:14:03 -0400

    removes react app

--- javascript/react/react-app/app/assets/javascripts/components/holding.js.jsx
+++
@@ -1,9 +0,0 @@
-
-const Holding = (props) => {
-    return (
-        <li style={ { border: "1px solid green" } }> 
-            Ticker: { props.ticker } Price: { props.price }
-            <button onClick={props.deleteHolding.bind(this, props.id)}>Delete</button>
-        </li>
-    )
-}
