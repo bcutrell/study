a855d9fc2a5f17c34ef4032eee0bfb2770c698c0
Ben Cutrell <bcutrell@dhcp-a-074.int.smartleaf.com>
2018-11-16 15:30:27 -0500

    adds notes

---
+++ python/data-science/projects/stock_market_analysis/regression.py
@@ -0,0 +1,2 @@
+# Regression Notes from Data Analysis and Visualization Course
+# http://nbviewer.jupyter.org/github/jmportilla/Udemy---Machine-Learning/blob/master/Supervised%20Learning%20-%20%20Linear%20Regression.ipynb
