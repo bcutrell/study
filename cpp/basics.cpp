3815fcc61cc25f7fa0cf3e34d25fb9a336af8b62
Ben Cutrell <bcutrell@dhcp-a-074.int.smartleaf.com>
2018-11-20 13:59:24 -0500

    simple variable examples

--- cpp/basics.cpp
+++ cpp/basics.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 int main()
 {
-  cout << "The eval of 2+2 is equal to 4";
+  // variables
+  int A = 4;
+  cout << A << endl;
+  cout << &A << endl;
+
+  A = 10;
+  cout << A << endl;
+  cout << &A;
+
   return 0;
 }
