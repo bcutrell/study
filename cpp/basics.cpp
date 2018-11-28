236ca6eac78d60e3a6063ab29a3e79391a124d0e
Ben Cutrell <bcutrell@dhcp-a-074.int.smartleaf.com>
2018-11-28 16:02:14 -0500

    more cpp basics

--- cpp/basics.cpp
+++ cpp/basics.cpp
@@ -2,16 +2,42 @@
 
 using namespace std;
 
-int main()
-{
-  // variables
-  int A = 4;
-  cout << A << endl;
-  cout << &A << endl;
+int main() {
+    // variables
+    int A = 4;
+    cout << A << endl;
+    cout << &A << endl;
 
-  A = 10;
-  cout << A << endl;
-  cout << &A;
+    A = 10;
+    cout << A << endl;
+    cout << &A << endl;
 
-  return 0;
+    // -32768 to 32767
+    short int t1 = 5; // or short t1=5;
+
+    float t2 = 5.12; // 4 bytes nr up to 38 zeros
+
+    double t3 = 5.12; // 8 bytes nr up to 308 zeros
+
+    char t4 = 'a'; // character
+
+    string t5 = "hello there";
+    string x = "hello";
+    string y = "there";
+    string xplusy = x + y;
+
+    bool t6 = true; // FALSE is always 0
+
+    // for unsigned short int 65535
+    unsigned short int t7 = 65535;
+
+    const string NAME = "can not change";
+
+    int a;
+    cin >> a; // console input
+
+    cout << a;
+
+    return 0;
 }
+
