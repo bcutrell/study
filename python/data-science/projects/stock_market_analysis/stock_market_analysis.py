47889fc145a66f0093289f1642338219844d2309
Ben Cutrell <bcutrell@dhcp-a-074.int.smartleaf.com>
2018-12-06 12:38:41 -0500

    adds graphing examples

--- python/data-science/projects/stock_market_analysis/stock_market_analysis.py
+++ python/data-science/projects/stock_market_analysis/stock_market_analysis.py
@@ -42,6 +42,49 @@ def set_ma(stock_df, ma_day=[10,20,50]):
 def set_daily_return(stock_df):
     stock_df['Daily Return'] = AAPL['Adj Close'].pct_change()
 
+def tech_summary():
+    closing_df = DataReader(['AAPL','GOOG','MSFT','AMZN'],'yahoo',start,end)['Adj Close']
+    tech_rets = closing_df.pct_change()
+
+    # from IPython.display import SVG
+    # SVG(url='http://upload.wikimedia.org/wikipedia/commons/d/d4/Correlation_examples2.svg')
+    sns.jointplot('GOOG','GOOG',tech_rets,kind='scatter',color='seagreen')
+    sns.jointplot('GOOG','MSFT',tech_rets,kind='scatter')
+    sns.pairplot(tech_rets.dropna())
+
+    # Set up our figure by naming it returns_fig, call PairPLot on the DataFrame
+    returns_fig = sns.PairGrid(tech_rets.dropna())
+
+    # Using map_upper we can specify what the upper triangle will look like.
+    returns_fig.map_upper(plt.scatter,color='purple')
+
+    # We can also define the lower triangle in the figure, inclufing the plot type (kde) or the color map (BluePurple)
+    returns_fig.map_lower(sns.kdeplot,cmap='cool_d')
+
+    # Finally we'll define the diagonal as a series of histogram plots of the daily return
+    returns_fig.map_diag(plt.hist,bins=30)
+
+    # Set up our figure by naming it returns_fig, call PairPLot on the DataFrame
+    returns_fig = sns.PairGrid(closing_df)
+
+    # Using map_upper we can specify what the upper triangle will look like.
+    returns_fig.map_upper(plt.scatter,color='purple')
+
+    # We can also define the lower triangle in the figure, inclufing the plot type (kde) or the color map (BluePurple)
+    returns_fig.map_lower(sns.kdeplot,cmap='cool_d')
+
+    # Finally we'll define the diagonal as a series of histogram plots of the closing price
+    returns_fig.map_diag(plt.hist,bins=30)
+
+    # Let's go ahead and use sebron for a quick correlation plot for the daily returns
+    sns.corrplot(tech_rets.dropna(),annot=True)
+
+    return tech_rets
+
+def tech_risk(tech_rets):
+    pass
+
+
 if __name__ == '__main__':
     collect_stock_data()
     print(AAPL)
