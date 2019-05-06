dc95ae2b9e662ecadd5b4dfc554395145843d2fe
Ben Cutrell <bcutrell@dhcp-a-074.int.smartleaf.com>
2019-05-06 15:45:26 -0400

    finishes statistical thinking part 1

--- python/statistical_thinking.py
+++ python/statistical_thinking.py
@@ -288,3 +288,116 @@ plt.ylabel('Y')
 # Show the plot
 plt.show()
 
+# Draw 10,000 samples out of Poisson distribution: samples_poisson
+samples_poisson = np.random.poisson(10, size=10000)
+
+# Print the mean and standard deviation
+print('Poisson:     ', np.mean(samples_poisson),
+                       np.std(samples_poisson))
+
+# Specify values of n and p to consider for Binomial: n, p
+n = [20, 100, 1000]
+p = [0.5, 0.1, 0.01]
+
+# Draw 10,000 samples for each n,p pair: samples_binomial
+for i in range(3):
+    print(n[i], p[i])
+    samples_binomial = np.random.binomial(n[i], p[i], size=10000)
+
+    # Print results
+    print('n =', n[i], 'Binom:', np.mean(samples_binomial),
+                                 np.std(samples_binomial))
+
+# When we have rare events (low p, high n), the Binomial distribution is Poisson. 
+# This has a single parameter, the mean number of successes per time interval, 
+# in our case the mean number of no-hitters per season.
+
+# Draw 10,000 samples out of Poisson distribution: n_nohitters
+n_nohitters = np.random.poisson(251/115, size=10000)
+
+# Compute number of samples that are seven or greater: n_large
+n_large = np.sum(n_nohitters >= 7)
+
+# Compute probability of getting seven or more: p_large
+p_large = n_large / len(n_nohitters)
+
+# Take a million samples out of the Normal distribution: samples
+samples = np.random.normal(mu, sigma, size=1000000)
+
+# Compute the fraction that are faster than 144 seconds: prob
+prob = np.sum(samples <= 144) / len(samples)
+
+# Print the result
+print('Probability of besting Secretariat:', prob)
+# Print the result
+print('Probability of seven or more no-hitters:', p_large)
+
+# Draw 100000 samples from Normal distribution with stds of interest: samples_std1, samples_std3, samples_std10
+samples_std1 = np.random.normal(20, 1, size=100000)
+samples_std3 = np.random.normal(20, 3, size=100000)
+samples_std10 = np.random.normal(20, 10, size=100000)
+
+# Make histograms
+plt.hist(samples_std1, bins=100, normed=True, histtype='step')
+plt.hist(samples_std3, bins=100, normed=True, histtype='step')
+plt.hist(samples_std10, bins=100, normed=True, histtype='step')
+
+# Make a legend, set limits and show plot
+_ = plt.legend(('std = 1', 'std = 3', 'std = 10'))
+plt.ylim(-0.01, 0.42)
+plt.show()
+
+# Generate CDFs
+x_std1, y_std1 = ecdf(samples_std1)
+x_std3, y_std3 = ecdf(samples_std3)
+x_std10, y_std10 = ecdf(samples_std10)
+
+# Plot CDFs
+plt.plot(x_std1, y_std1, marker='.', linestyle='none')
+plt.plot(x_std3, y_std3, marker='.', linestyle='none')
+plt.plot(x_std10, y_std10, marker='.', linestyle='none')
+
+# Make a legend and show the plot
+_ = plt.legend(('std = 1', 'std = 3', 'std = 10'), loc='lower right')
+plt.show()
+
+# Compute mean and standard deviation: mu, sigma
+mu = np.mean(belmont_no_outliers)
+sigma = np.std(belmont_no_outliers)
+
+# Sample out of a normal distribution with this mu and sigma: samples
+samples = np.random.normal(mu, sigma, size=10000)
+
+# Get the CDF of the samples and of the data
+x_theor, y_theor = ecdf(samples)
+x, y = ecdf(belmont_no_outliers)
+
+# Plot the CDFs and show the plot
+_ = plt.plot(x_theor, y_theor)
+_ = plt.plot(x, y, marker='.', linestyle='none')
+_ = plt.xlabel('Belmont winning time (sec.)')
+_ = plt.ylabel('CDF')
+plt.show()
+
+def successive_poisson(tau1, tau2, size=1):
+    """Compute time for arrival of 2 successive Poisson processes."""
+    # Draw samples out of first exponential distribution: t1
+    t1 = np.random.exponential(tau1, size=size)
+
+    # Draw samples out of second exponential distribution: t2
+    t2 = np.random.exponential(tau2, size=size)
+
+    return t1 + t2
+
+# Draw samples of waiting times: waiting_times
+waiting_times = successive_poisson(764,715, size=100000)
+
+# Make the histogram
+plt.hist(waiting_times, bins=100, normed=True, histtype='step')
+
+# Label axes
+plt.xlabel('no hitter')
+plt.ylabel('cycle')
+
+# Show the plot
+plt.show()
