57f957c280dfbda878fd04bf56e74f28ec0da41b
Ben Cutrell <bcutrell@dhcp-a-074.int.smartleaf.com>
2019-10-03 09:21:18 -0400

    finishes portfolio analysis course

--- datacamp/portfolio_analysis.py
+++ datacamp/portfolio_analysis.py
@@ -51,9 +51,279 @@ port_standard_dev = np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights)))
 # Print the results
 print(str(np.round(port_standard_dev, 4) * 100) + '%')
 
+#
+# Risk and Return
+#
+
+# Calculate total rate of return from start to end
+total_return = (pf_AUM[-1] - pf_AUM[0]) / pf_AUM[0]
+
+# Annualize return
+annualized_return = ((1 + total_return)**(12/months))-1
+print (annualized_return)
+
+# Calculate the total return from the S&P500 value series
+total_return = (sp500_value[-1] - sp500_value[0]) / sp500_value[0]
+print(total_return)
+
+# Annualize the total return spanning 4 years
+annualized_return = ((1 + total_return)**(1/4))-1
+print (annualized_return)
+
+#
+# Risk Adjusted Returns
+#
+
+# Calculate total return and annualized return from price data
+total_return = (sp500_value[-1] - sp500_value[0]) / sp500_value[0]
+
+# Annualize the total return over 4 year
+annualized_return = ((1 + total_return)**(1/4))-1
+
+# Create the returns data
+returns_sp500 = sp500_value.pct_change()
+
+# Calculate annualized volatility from the standard deviation
+vol_sp500 = returns_sp500.std() * np.sqrt(250)
+
+# Calculate the Sharpe ratio
+sharpe_ratio = ((annualized_return - rfr) / vol_sp500)
+print (sharpe_ratio)
+
+# Calculate total return and annualized return from price data
+total_return = (pf_AUM[-1] - pf_AUM[0]) / pf_AUM[0]
+
+# Annualize the total return over 4 year
+annualized_return = ((1 + total_return)**(12/months))-1
+
+# Create the returns data
+pf_returns = pf_AUM.pct_change()
+
+# Calculate annualized volatility from the standard deviation
+vol_pf = pf_returns.std()*np.sqrt(250)
+
+# Calculate the Sharpe ratio
+sharpe_ratio = ((annualized_return - rfr) / vol_pf)
+print (sharpe_ratio)
+
+#
+# Non normal return distributions
+#
+
+# A negative skew means there is a 'bump to the right' compared to the normal distribution
+
+# Print the mean
+print("mean : ", returns_sp500.mean()*100)
+
+# Print the standard deviation
+print("Std. dev  : ", returns_sp500.std()*100)
+
+# Print the skewness
+print("skew : ", returns_sp500.skew())
+
+# Print the kurtosis
+print("kurt : ", returns_sp500.kurtosis())
+
+#
+# Alternative measures of risk
+#
+
+# Create a downside return column with the negative returns only
+downside_returns = df.loc[df['pf_returns'] < target]
+
+# Calculate expected return and std dev of downside
+expected_return = df['pf_returns'].mean()
+down_stdev = downside_returns['pf_returns'].std()
+
+# Calculate the sortino ratio
+sortino_ratio = (expected_return - rfr)/down_stdev
+
+# Print the results
+print("Expected return  : ", expected_return*100)
+print("Downside risk   : ", down_stdev*100)
+print("Sortino ratio : ", sortino_ratio)
+
+# Calculate the max value
+roll_max = df.rolling(center=False,min_periods=1,window=252).max()
+
+# Calculate the daily draw-down relative to the max
+daily_draw_down = df/roll_max - 1.0
+
+# Calculate the minimum (negative) daily draw-down
+max_daily_draw_down = daily_draw_down.rolling(center=False,min_periods=1,window=252).min()
+
+# Plot the results
+plt.figure(figsize=(15,15))
+plt.plot(date, daily_draw_down, label='Daily drawdown')
+plt.plot(date, max_daily_draw_down, label='Maximum daily drawdown in time-window')
+plt.legend()
+plt.show()
+
+# Calculate return of the portfolio
+total_return_pf = (portfolio_data['pf_weights']*portfolio_data['mean_return']).sum()
+
+# Calculate return of the benchmark
+total_return_bm = (portfolio_data['bm_weights']*portfolio_data['mean_return']).sum()
+
+# Calculate and print the active return
+active_return = total_return_pf - total_return_bm
+print ("%.2f%%" % active_return)
+
+# Print the sum of the bm and pf weights
+print (portfolio_data.bm_weights.sum())
+print (portfolio_data.pf_weights.sum())
+
+# Group dataframe by GICS sectors
+grouped_df=portfolio_data.groupby('GICS Sector').sum()
+
+# Calculate active weights of portfolio
+grouped_df['active_weight']=grouped_df['pf_weights']-grouped_df['bm_weights']
+print (grouped_df['active_weight'])
+
+#
+# Risk Factors
+#
+
+# Calculate 20-day rolling correlation with momentum
+df['correlation_mom']=df['sp500'].rolling(20).corr(df['momentum'])
+
+# Calculate 20-day rolling correlation with value
+df['correlation_value']=df['sp500'].rolling(20).corr(df['value'])
+
+# Plot the correlations
+df['correlation_mom'].plot()
+df['correlation_value'].plot()
+plt.legend()
+plt.show()
+
+# Calculate the pairwise correlation
+factor_data.corr()
+
+# Calculate rolling 5 day correlation
+factor_data['correlation_value']=factor_data['portfolio'].rolling(5).corr(factor_data['value'])
+
+# Plot the rolling correlation
+factor_data['correlation_value'].plot()
+plt.legend()
+plt.show()
+
+# Define the model
+model = sm.OLS(factor_returns['pf_returns'], factor_returns[['Mkt-RF','SMB', 'HML']]).fit()
+
+# Create model predictions
+predictions = model.predict(factor_returns[['Mkt-RF','SMB', 'HML']])
+
+# Print out the summary
+model.summary()
+
+# Import statsmodels
+import statsmodels.api as sm
+
+# Obtain the beta coefficients
+b1, b2, b3 = sm.OLS(factor_returns['pf_returns'], factor_returns[['Mkt-RF','SMB', 'HML']]).fit().params
+
+# Print the betas
+print ('Sensitivities of active returns to factors:\nMkt-Rf: %f\nSMB: %f\nHML: %f' %  (b1, b2, b3))
+
+#
+# Quantopian - Pyfolio
+#
+
+# Set the index to datetime
+returns_sp500.index=pd.to_datetime(returns_sp500.index)
+
+# Ensure the returns are a series
+returns=returns_sp500['S&P500']
+
+# Create the returns tear sheet
+fig = pf.create_returns_tear_sheet(returns, return_fig=True)
+
+# Display a zoomed in version of the tear sheet
+display_tear_sheet()
+
+# Define sector mappings
+sect_map = {'COST': 'Consumer Goods',
+            'INTC': 'Technology',
+            'CERN': 'Healthcare',
+            'GPS': 'Technology',
+            'MMM': 'Construction',
+            'DELL': 'Technology',
+            'AMD': 'Technology'}
+
+# Create sector exposure tear sheet
+pf.create_position_tear_sheet(returns, positions, sector_mappings=sect_map)
+display_tear_sheet()
+
+#
+# Modern Portfolio Theory
+#
+
+# Import the packages
+from pypfopt import risk_models
+from pypfopt import expected_returns
+from pypfopt.efficient_frontier import EfficientFrontier
+
+# Calculate expected returns mu
+mu = expected_returns.mean_historical_return(stock_prices)
+
+# Calculate the covariance matrix S
+Sigma = risk_models.sample_cov(stock_prices)
+
+# Obtain the efficient frontier
+ef = EfficientFrontier(mu, Sigma)
+print(mu, Sigma)
+
+# Get the returns from the stock price data
+returns=stock_prices.pct_change()
+
+# Calculate the annualized covariance matrix
+covMatrix = returns.cov()*252
+
+# Calculate the covariance matrix Sigma from a`PyPortfolioOpt` function
+Sigma = risk_models.sample_cov(stock_prices)
+
+# Print both covariance matrices
+print (covMatrix, Sigma)
+
+# Get the minimum risk portfolio for a target return
+weights = ef.efficient_return(0.2)
+print (weights)
+
+# Show portfolio performance
+ef.portfolio_performance(verbose=True)
+
+# Define the efficient frontier
+ef = EfficientFrontier(mu, Sigma)
+
+# Calculate weights for the maximum Sharpe ratio portfolio
+raw_weights_maxsharpe = ef.max_sharpe()
+cleaned_weights_maxsharpe = ef.clean_weights()
+print (raw_weights_maxsharpe, cleaned_weights_maxsharpe)
+
+# Calculate weights for the minimum volatility portfolio
+raw_weights_minvol = ef.min_volatility()
+cleaned_weights_minvol = ef.clean_weights()
+
+# Show portfolio performance
+print(cleaned_weights_minvol)
+ef.portfolio_performance(verbose=True)
+
+# Define exponentially weightedSigma and mu using stock_prices
+Sigma = risk_models.exp_cov(stock_prices, span=180, frequency=252)
+mu = expected_returns.ema_historical_return(stock_prices, frequency=252, span=180)
+
+# Calculate the efficient frontier
+ef = EfficientFrontier(mu, Sigma)
+
+# Calculate weights for the maximum sharpe ratio optimization
+raw_weights_maxsharpe = ef.max_sharpe()
+
+# Show portfolio performance
+ef.portfolio_performance(verbose=True)
+
 # datacamp hack
 import inspect
-print(inspect.getsource(function)
+print(inspect.getsource(function))
 
 def plot_pct_returns(df):
     fig, ax = plt.subplots()
