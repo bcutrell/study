635d354261d73785c2bff026ec45bdb92ee72827
Ben Cutrell <bcutrell@apple-mbp.mynetworksettings.com>
2023-08-30 15:03:10 -0400

    Adds iris example

--- udemy/pytorch-for-deep-learning/notes.py
+++ udemy/pytorch-for-deep-learning/notes.py
@@ -205,3 +205,89 @@ for i, ax in enumerate(axes.flat):
 
 fig.legend(labels=labels, loc=3, bbox_to_anchor=(1.0,0.85))
 plt.show()
+
+X = df.drop('target',axis=1).values
+y = df['target'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=33)
+
+X_train = torch.FloatTensor(X_train)
+X_test = torch.FloatTensor(X_test)
+y_train = torch.LongTensor(y_train)
+y_test = torch.LongTensor(y_test)
+
+trainloader = DataLoader(X_train, batch_size=60, shuffle=True)
+
+testloader = DataLoader(X_test, batch_size=60, shuffle=False)
+
+torch.manual_seed(4)
+model = Model()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+# Train the model
+epochs = 100
+losses = []
+
+for i in range(epochs):
+    i+=1
+    y_pred = model.forward(X_train)
+    loss = criterion(y_pred, y_train)
+    losses.append(loss)
+
+    # a neat trick to save screen space:
+    if i%10 == 1:
+        print(f'epoch: {i:2}  loss: {loss.item():10.8f}')
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+plt.plot(range(epochs), losses)
+plt.ylabel('Loss')
+plt.xlabel('epoch');
+
+# Validate the model
+with torch.no_grad():
+    y_val = model.forward(X_test)
+    loss = criterion(y_val, y_test)
+print(f'{loss:.8f}')
+
+correct = 0
+with torch.no_grad():
+    for i,data in enumerate(X_test):
+        y_val = model.forward(data)
+        print(f'{i+1:2}. {str(y_val):38}  {y_test[i]}')
+        if y_val.argmax().item() == y_test[i]:
+            correct += 1
+print(f'\n{correct} out of {len(y_test)} = {100*correct/len(y_test):.2f}% correct')
+
+torch.save(model.state_dict(), 'IrisDatasetModel.pt')
+
+# Unseen Data
+mystery_iris = torch.tensor([5.6,3.7,2.2,0.5])
+fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(10,7))
+fig.tight_layout()
+
+plots = [(0,1),(2,3),(0,2),(1,3)]
+colors = ['b', 'r', 'g']
+labels = ['Iris setosa','Iris virginica','Iris versicolor','Mystery iris']
+
+for i, ax in enumerate(axes.flat):
+    for j in range(3):
+        x = df.columns[plots[i][0]]
+        y = df.columns[plots[i][1]]
+        ax.scatter(df[df['target']==j][x], df[df['target']==j][y], color=colors[j])
+        ax.set(xlabel=x, ylabel=y)
+
+    # Add a plot for our mystery iris:
+    ax.scatter(mystery_iris[plots[i][0]],mystery_iris[plots[i][1]], color='y')
+
+fig.legend(labels=labels, loc=3, bbox_to_anchor=(1.0,0.85))
+plt.show()
+
+with torch.no_grad():
+    print(new_model(mystery_iris))
+    print()
+    print(labels[new_model(mystery_iris).argmax()])
