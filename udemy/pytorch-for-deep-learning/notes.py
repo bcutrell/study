54a216cacb95cdd6efba217dbe717bcd16ddbe8b
Ben Cutrell <bcutrell@Apple-MacBook-Pro.local>
2023-09-12 20:33:03 -0400

    continues ANN exercises

--- udemy/pytorch-for-deep-learning/notes.py
+++ udemy/pytorch-for-deep-learning/notes.py
@@ -326,3 +326,66 @@ cats = torch.tensor(cats, dtype=torch.int64)
 
 conts = np.stack([df[col].values for col in cont_cols], 1)
 conts[:5]
+
+conts = torch.tensor(conts, dtype=torch.float)
+
+# create a label tensor
+y = torch.tensor(df[y_col].values).flatten()
+
+b = 30000 # suggested batch size
+t = 5000  # suggested test size
+
+cat_train = cats[:b-t]
+cat_test  = cats[b-t:b]
+con_train = conts[:b-t]
+con_test  = conts[b-t:b]
+y_train   = y[:b-t]
+y_test    = y[b-t:b]
+
+# Define Model Class
+class TabularModel(nn.Module):
+
+    def __init__(self, emb_szs, n_cont, out_sz, layers, p=0.5):
+        # Call the parent __init__
+        super().__init__()
+
+        # Set up the embedding, dropout, and batch normalization layer attributes
+        self.embeds = nn.ModuleList([nn.Embedding(ni, nf) for ni,nf in emb_szs])
+        self.emb_drop = nn.Dropout(p)
+        self.bn_cont = nn.BatchNorm1d(n_cont)
+
+        # Assign a variable to hold a list of layers
+        layerlist = []
+
+        # Assign a variable to store the number of embedding and continuous layers
+        n_emb = sum((nf for ni,nf in emb_szs))
+        n_in = n_emb + n_cont
+
+        # Iterate through the passed-in "layers" parameter (ie, [200,100]) to build a list of layers
+        for i in layers:
+            layerlist.append(nn.Linear(n_in,i))
+            layerlist.append(nn.ReLU(inplace=True))
+            layerlist.append(nn.BatchNorm1d(i))
+            layerlist.append(nn.Dropout(p))
+            n_in = i
+        layerlist.append(nn.Linear(layers[-1],out_sz))
+
+        # Convert the list of layers into an attribute
+        self.layers = nn.Sequential(*layerlist)
+
+    def forward(self, x_cat, x_cont):
+        # Extract embedding values from the incoming categorical data
+        embeddings = []
+        for i,e in enumerate(self.embeds):
+            embeddings.append(e(x_cat[:,i]))
+        x = torch.cat(embeddings, 1)
+        # Perform an initial dropout on the embeddings
+        x = self.emb_drop(x)
+
+        # Normalize the incoming continuous data
+        x_cont = self.bn_cont(x_cont)
+        x = torch.cat([x, x_cont], 1)
+
+        # Set up model layers
+        x = self.layers(x)
+        return x
