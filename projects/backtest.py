915160f7775f6d73198355ef951a6e512182625b
Ben Cutrell <bcutrell@apple-mbp.mynetworksettings.com>
2023-10-10 14:09:05 -0400

    Fixes vectorbt and backtrader examples

--- projects/backtest.py
+++ projects/backtest.py
@@ -1,35 +1,56 @@
-import datetime
+from datetime import datetime
+import argparse
+
+import yfinance as yf
 import backtrader as bt
 import vectorbt as vbt
 
+
+
+#
+# Vectorbt
+#
 def vectorbt_example():
     # Download daily close data for Apple stock
     data = vbt.YFData.download('AAPL', interval='1d')
 
     # Calculate the 10-day and 50-day moving averages
-    ma_fast = vbt.MA.run(data['Close'], 10)
-    ma_slow = vbt.MA.run(data['Close'], 50)
+    ma_fast = vbt.MA.run(data.data['AAPL']['Close'], 10)
+    ma_slow = vbt.MA.run(data.data['AAPL']['Close'], 50)
 
     # Generate buy and sell signals based on the moving average crossover
     entries = ma_fast.ma_crossed_above(ma_slow)
     exits = ma_fast.ma_crossed_below(ma_slow)
 
     # Create a portfolio object and run the backtest
-    portfolio = vbt.Portfolio.from_signals(data['Close'], entries, exits, freq='d')
-    results = portfolio.run()
+    portfolio = vbt.Portfolio.from_signals(data.data['AAPL']['Close'], entries, exits, freq='d')
+    print(portfolio.total_profit())
 
-    # Print the backtest results
-    print(results.stats())
-
+#
+# Backtrader
+#
 class BuyAndHoldTarget(bt.Strategy):
+
     def start(self):
+        """ set the starting cash """
         self.val_start = self.broker.get_cash()  # keep the starting cash
 
+    def log(self, txt, dt=None):
+        """ Logging function for this strategy """
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
     def nextstart(self):
+        """ called exactly once for each data, after the ``next`` is called for the 1st time """
         # Buy all the available cash
         size = int(self.broker.get_cash() / self.data)
         self.buy(size=size)
-        self._private = 10
+
+    def next(self):
+        """ called every period """
+        # log the closing price of the series from the reference
+        if self.data.tick_last:
+            self.log('Tick Last, %.2f' % self.data.tick_last)
 
     def stop(self):
         # calculate the actual returns
@@ -38,7 +59,6 @@ class BuyAndHoldTarget(bt.Strategy):
 
     def notify_timer(self, timer, when, *args, **kwargs):
         # Add the influx of monthly cash to the broker
-        print(notify_timer)
         self.broker.add_cash(10)
 
         # buy available cash
@@ -47,13 +67,24 @@ class BuyAndHoldTarget(bt.Strategy):
 
 def backtrader_example():
     cerebro = bt.Cerebro()
-    data = bt.feeds.BacktraderCSVData(dataname="../data/data.txt")
-    cerebro.adddata(data)
+
+    # Add data from a file
+    # data = bt.feeds.BacktraderCSVData(dataname="../data/data.txt")
+    # cerebro.adddata(data)
+
+    # Add a data feed for AAPL
+    aapl_data = bt.feeds.PandasData(dataname=yf.download('AAPL', '2022-01-01', '2023-01-01'))
+    cerebro.adddata(aapl_data)
+
+    # Add a data feed for MSFT
+    msft_data = bt.feeds.PandasData(dataname=yf.download('MSFT', '2022-01-01', '2023-01-01'))
+    cerebro.adddata(msft_data)
+
     cerebro.addstrategy(BuyAndHoldTarget)
     cerebro.broker = bt.brokers.BackBroker()
     cerebro.addsizer(bt.sizers.FixedSize)
     cerebro.run()
-    cerebro.plot()
+    # cerebro.plot()
 
 def main():
     backtrader_example()
