dc124d1d396dd4207f33bac8c1eee7d242e7df7b
Ben Cutrell <bcutrell@apple-mbp.mynetworksettings.com>
2024-02-05 08:21:40 -0500

    Adds resources

--- projects/textual_file_monitor/app.py
+++ projects/textual_file_monitor/app.py
@@ -1,6 +1,20 @@
+"""
+Trying to learn some Textual...
+
+Awesome libraries
+    mysql monitor -> https://github.com/charles-001/dolphie
+    cooler $ top  -> https://github.com/nschloe/tiptop
+    SQL ide       -> https://github.com/tconbeer/harlequin
+
+Resources
+https://dev.to/wiseai/textual-the-definitive-guide-part-3-2gl
+https://textual.textualize.io/styles/grid/#__tabbed_1_1
+"""
 from textual.app import App, ComposeResult
 from textual.containers import ScrollableContainer
-from textual.widgets import Button, Footer, Header, DataTable
+from textual.widgets import Button, Footer, Header, DataTable, TabbedContent, Tab
+from textual.containers import Horizontal, Vertical
+from textual.binding import Binding
 
 ROWS = [
     ("task", "time", "status"),
@@ -9,17 +23,44 @@ ROWS = [
     ("Fix bugs", "2h", "in progress"),
 ]
 
+class Menu(TabbedContent, can_focus=True):
+    BORDER_TITLE = "Data Catalog"
+
+    BINDINGS = [
+        Binding("j", "switch_tab(-1)", "Previous Tab", show=False),
+        Binding("k", "switch_tab(1)", "Next Tab", show=False),
+    ]
+
+    def compose(self) -> ComposeResult:
+        yield Tab("Tables", id="tables")
+        yield Tab("Views", id="views")
+        yield Tab("Procedures", id="procedures")
+        yield Tab("Functions", id="functions")
+
+    def on_mount(self) -> None:
+        self.border_title = "Data Catalog"
+
 class MyApp(App):
     """ A Textual App to monitor files. """
+
+    CSS_PATH = "style.tcss"
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
         yield Header()
         yield DataTable()
+
+        # with Horizontal():
+        #     yield Menu()
+        #     with Vertical(id="main"):
+        #         yield DataTable()
+
         yield Footer()
 
     def on_mount(self) -> None:
         table = self.query_one(DataTable)
+        table.border_title = "Data Catalog"
+        table.zebra_stripes = True
         table.add_columns(*ROWS[0])
         table.add_rows(ROWS[1:])
 
